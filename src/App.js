519548484e62f0bd3afda557115a38cdc3070f43
Gabriel Ferguson <gferguson954@gmail.com>
2023-08-25 17:28:47 -0500

    pokedex search bug fix and pokemon detail card

--- src/App.js
+++ src/App.js
@@ -1,67 +1,157 @@
-import { useEffect, useState } from "react";
-import { fetchAllPokemon } from "./api";
+import { useState, useMemo } from 'react';
+import {
+    fetchAllPokemon,
+    fetchPokemonDetailsByName,
+    fetchEvolutionChainById,
+    fetchPokemonSpeciesByName,
+} from './api';
 
 function App() {
-    const [pokemonIndex, setPokemonIndex] = useState([])
-    const [pokemon, setPokemon] = useState([])
-    const [searchValue, setSearchValue] = useState('')
-    const [pokemonDetails, setPokemonDetails] = useState()
+    const [pokemon, setPokemon] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
+    const [pokemonDetails, setPokemonDetails] = useState();
+    const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
+    // fetch initial list of all pokemon only once
+    useMemo(() => {
         const fetchPokemon = async () => {
-            const {results: pokemonList} = await fetchAllPokemon()
+            setIsLoading(true);
+            const { results: pokemonList } = await fetchAllPokemon();
 
-            setPokemon(pokemonList)
-            setPokemonIndex(pokemonList)
+            setPokemon(pokemonList);
+        };
+
+        fetchPokemon().finally(() => setIsLoading(false));
+    }, []);
+
+    const pokemonList = useMemo(() => {
+        if (searchValue && pokemon.length) {
+            return pokemon.filter(({ name }) => name.includes(searchValue.toLowerCase()));
         }
 
-        fetchPokemon().then(() => {
-            /** noop **/
-        })
-    }, [searchValue])
+        return pokemon;
+    }, [searchValue, pokemon]);
 
     const onSearchValueChange = (event) => {
-        const value = event.target.value
-        setSearchValue(value)
+        const value = event.target.value;
+        setSearchValue(value);
+    };
 
-        setPokemon(
-            pokemonIndex.filter(monster => !monster.name.includes(value))
-        )
-    }
+    const getPokemonEvolutions = async (evolutionChainId) => {
+        const { chain } = await fetchEvolutionChainById(evolutionChainId);
+        const evolutionNames = [];
+
+        //recursively add evolution names to array
+        const compileEvolutionNames = (pokemonEvolutionChain, isFirstEvolution = true) => {
+            if (pokemonEvolutionChain.species && isFirstEvolution) {
+                evolutionNames.push(pokemonEvolutionChain.species.name);
+            }
+
+            if (pokemonEvolutionChain.evolves_to.length) {
+                const evolution = pokemonEvolutionChain.evolves_to[0];
+                evolutionNames.push(evolution.species.name);
+
+                if (evolution.evolves_to.length) {
+                    compileEvolutionNames(evolution, false);
+                }
+            }
+        };
+
+        compileEvolutionNames(chain);
+
+        return evolutionNames;
+    };
+
+    const fetchPokemonInfo = async (name) => {
+        const { moves, types, name: pokemonName } = await fetchPokemonDetailsByName(name);
+        const { evolution_chain } = await fetchPokemonSpeciesByName(pokemonName);
+        //have to get evolution chain id off of url as api doesn't have a property for it as far as I can find
+        const evolutionChainSplitUrl = evolution_chain.url.slice(0, -1).split('/');
+        const evolutionChainId = evolutionChainSplitUrl[evolutionChainSplitUrl.length - 1];
+        const evolutions = await getPokemonEvolutions(evolutionChainId);
+
+        return {
+            evolutions,
+            pokemonName,
+            moves: moves.map(({ move }) => move.name),
+            types: types.map(({ type }) => type.name),
+        };
+    };
 
     const onGetDetails = (name) => async () => {
-        /** code here **/
-    }
+        setPokemonDetails(null);
+
+        const { evolutions, pokemonName, moves, types } = await fetchPokemonInfo(name);
+
+        setPokemonDetails({
+            evolutions,
+            pokemonName,
+            moves,
+            types,
+        });
+    };
+
+    const renderPokemonStats = (pokedexColumnType) => {
+        const columnTypeMap = {
+            types: {
+                title: 'Types',
+                statList: pokemonDetails[pokedexColumnType],
+            },
+            moves: {
+                title: 'Moves',
+                statList: pokemonDetails[pokedexColumnType],
+            },
+        };
+
+        const { title, statList } = columnTypeMap[pokedexColumnType];
+
+        return (
+            <div>
+                <p className='bold'>{title}</p>
+                <ul className='pokedex__types-moves-list-container'>
+                    {statList.map((pokemonStat) => {
+                        return <li key={pokemonStat}>{pokemonStat}</li>;
+                    })}
+                </ul>
+            </div>
+        );
+    };
 
     return (
         <div className={'pokedex__container'}>
             <div className={'pokedex__search-input'}>
-                <input value={searchValue} onChange={onSearchValueChange} placeholder={'Search Pokemon'}/>
+                <input value={searchValue} onChange={onSearchValueChange} placeholder={'Search Pokemon'} />
             </div>
             <div className={'pokedex__content'}>
-                {pokemon.length > 0 && (
+                {pokemonList.length > 0 ? (
                     <div className={'pokedex__search-results'}>
-                        {
-                            pokemon.map(monster => {
-                                return (
-                                    <div className={'pokedex__list-item'} key={monster.name}>
-                                        <div>
-                                            {monster.name}
-                                        </div>
-                                        <button onClick={onGetDetails(monster.name)}>Get Details</button>
-                                    </div>
-                                )
-                            })
-                        }
+                        {pokemonList.map((monster) => {
+                            return (
+                                <div className={'pokedex__list-item'} key={monster.name}>
+                                    <div>{monster.name}</div>
+                                    <button onClick={onGetDetails(monster.name)}>Get Details</button>
+                                </div>
+                            );
+                        })}
+                    </div>
+                ) : (
+                    <p>{isLoading ? 'Loading...' : 'No Results Found'}</p>
+                )}
+                {pokemonDetails && (
+                    <div className={'pokedex__details'}>
+                        <p className='bold'>{pokemonDetails.pokemonName}</p>
+                        <div className={'pokedex__types-moves'}>
+                            {renderPokemonStats('types')}
+                            {renderPokemonStats('moves')}
+                        </div>
+                        <p className='bold'>Evolutions</p>
+                        <div className='pokedex__evolution-list'>
+                            {pokemonDetails.evolutions.map((evolutionName) => (
+                                <p key={evolutionName}>{evolutionName}</p>
+                            ))}
+                        </div>
                     </div>
                 )}
-                {
-                    pokemonDetails && (
-                        <div className={'pokedex__details'}>
-                            {/*  code here  */}
-                        </div>
-                    )
-                }
             </div>
         </div>
     );
